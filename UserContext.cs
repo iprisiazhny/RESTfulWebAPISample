228559c922536ee8b178d12e5edc31a855408ac1
Rusten Line <rusten.line@ga.gov.au>
2022-06-13 18:18:36 +0400

    Add project files.

---
+++ UserContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RESTfulWebAPISample
+{
+    public class UserContext: DbContext 
+    {
+        public UserContext()
+        {}
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlServer("server=RustenUltraBook;Database=UserInfo;Trusted_Connection=True;");
+        }
+    }
+}
